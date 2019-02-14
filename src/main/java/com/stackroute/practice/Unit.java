afb504404982c2b55f551ce2ebd10569265643f3
abhinavfzr <abhi.king11@gmail.com>
2019-02-14 15:44:12 +0530

    first commit

---
+++ src/main/java/com/stackroute/practice/Unit.java
@@ -0,0 +1,43 @@
+package com.stackroute.practice;
+
+public class Unit
+{
+    String palindromeCheck(String str)  //test 1 que
+    {
+        return null;
+    }
+    int  isPower(int number)
+    {
+        return 0;
+    }
+    boolean isEven(int number)
+    {
+        return true;
+    }
+    int isFact(int number)
+    {
+        return 0;
+    }
+    int[] isGrades(int grades[],int n)
+    {
+
+        return null;
+    }
+    String contentPrintTextFile(String filename)
+    {
+        return null;
+    }
+    int contentOfFileTimes(String filename)
+    {
+        return 0;
+    }
+    String[]  memberData(String name,int age, double Salary)
+    {
+        return  null;
+    }
+    byte[] byteFileContent(String Filename)
+    {
+      return null;
+    }
+    
+}
